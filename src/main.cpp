74f4bebece22b039675898226d979ebf9e7c1b96
Sanek Sergeev <se.sanek@yandex.com>
2026-01-13 22:44:07 +0500

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,42 @@ Disp1637Colon disp(DIO_PIN, CLK_PIN);
 
 #include <GyverDS18.h>
 
-GyverDS18Single ds(12);  // пин
+GyverDS18Single dso(12);  // пин
+
+#include <GyverDS18Array.h>
+
+uint64_t addr[] = {
+    0x4D0417508099FF28,
+    0xFE04175159CDFF28,
+    0xC3041750E553FF28,
+    0xCF0417505B78FF28,
+};
+GyverDS18Array dsi(5, addr, 4);
 
 void setup() {
-    ds.setResolution(12);
+  dso.setResolution(9);
+  dsi.setResolution(12);
   disp.printRight(true);
 }
 void loop() {
-    if (!ds.tick()) {
-      int temp1 = ds.getTemp()*10;
-      disp.clear();
-      disp.print(temp1);
-      disp.setCursor(3);
-      disp.print('*');
-      disp.update();
+  static int tempOut;
+  static int tempIn;
+    if (!dso.tick()) {
+      tempOut = dso.getTemp()*10;
     }
+    if (!dsi.tick()) {
+        
+        float temps[4];
+        dsi.readTemps(temps);
+        for (float t : temps) {
+          tempIn = 0;
+          tempIn += t;
+        }
+        tempIn *= 0.25;
+    }
+    disp.clear();
+    disp.print(tempIn);
+    disp.setCursor(3);
+    disp.print('*');
+    disp.update();
 }
